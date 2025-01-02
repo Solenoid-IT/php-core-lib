c871ad631d54e58d8a1470554e0f58b936e2e58c
CapitanPNG <pierniccologiannelli@gmail.com>
2025-01-02 18:51:36 +0000

    store class

--- src/Store.php
+++ src/Store.php
@@ -8,7 +8,23 @@ namespace Solenoid\Core;
 
 class Store
 {
-    
+    private array $values = [];
+
+
+
+    # Returns [mixed|false]
+    protected function get (string $id)
+    {
+        // Returning the value
+        return $this->values[ $id ] ?? false;
+    }
+
+    # Returns [void]
+    protected function set (string $id, mixed $value)
+    {
+        // (Getting the value)
+        $this->values[ $id ] = $value;
+    }
 }
 
 
